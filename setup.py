fed7240a9fdd04ce5bb417f3765b5de042906522
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-08-17 11:44:15 +0300

    Fix

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from distutils.version import LooseVersion
 import pip
 from setuptools import find_packages, setup
 
-if LooseVersion(sys.version) < LooseVersion("3.8.3"):
+if LooseVersion(sys.version) < LooseVersion("3.6"):
     raise RuntimeError(
-        "Tensorflow TTS requires python >= 3.8.3, "
+        "Tensorflow TTS requires python >= 3.6, "
         "but your Python version is {}".format(sys.version)
     )
 
-if LooseVersion(pip.__version__) < LooseVersion("21.2.4"):
+if LooseVersion(pip.__version__) < LooseVersion("19"):
     raise RuntimeError(
         "pip>=19.0.0 is required, but your pip version is {}. "
         'Try again after "pip install -U pip"'.format(pip.__version__)
@@ -89,8 +89,8 @@ setup(
     extras_require=extras_require,
     entry_points=entry_points,
     classifiers=[
-        "Programming Language :: Python :: 3.8.3",
-        "Programming Language :: Python :: 3.8.3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: Apache Software License",
